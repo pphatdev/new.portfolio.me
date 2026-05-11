3fd5389aadd4016fdc32ee2a2c8c6411d3281a28
pphatdev <pphat.kits@gmail.com>
2026-05-11 08:35:43 +0700

    feat: initialize project structure with Tailwind CSS, Framer Motion, and schema-org personal data support

---
+++ src/shared/data/index.ts
@@ -0,0 +1,11 @@
+import * as faqJson from './faq.json';
+import * as contactJson from './contact.json';
+
+export const appName = "LEAT Sophat";
+
+export const appDescriptions = "I started my career as a Front-end Developer in 2021, and I have a passion for creating beautiful and functional user interfaces. I love to learn new technologies and improve my skills every day.\n\nI am also a big fan of open-source projects and I enjoy contributing to the community.";
+
+export const appPositions = ["Senior Front-end Developer.", "UI/UX Designer."];
+
+export const faqData = faqJson;
+export const contactData = contactJson;
