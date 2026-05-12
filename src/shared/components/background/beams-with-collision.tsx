a5b4110fa74a4f20530f0c6b18454d9f021e7bc4
pphatdev <pphat.kits@gmail.com>
2026-05-13 00:06:48 +0700

    feat: implement hero section with canvas animation, skill icons, and supporting UI components

--- src/shared/components/background/beams-with-collision.tsx
+++ src/shared/components/background/beams-with-collision.tsx
@@ -1,64 +1,21 @@
 "use client";
-import { cn } from "@/shared/libs/utils";
 import { motion, AnimatePresence } from "framer-motion";
 import React, { useRef, useState, useEffect, forwardRef } from "react";
+import { cn } from "@/shared/libs/utils";
+
+const beams = [
+    { initialX: 10, translateX: 10, duration: 7, repeatDelay: 3, delay: 2, },
+    { initialX: 600, translateX: 600, duration: 3, repeatDelay: 3, delay: 4, },
+    { initialX: 100, translateX: 100, duration: 7, repeatDelay: 7, className: "h-6", },
+    { initialX: 400, translateX: 400, duration: 5, repeatDelay: 14, delay: 4, },
+    { initialX: 800, translateX: 800, duration: 11, repeatDelay: 2, className: "h-20", },
+    { initialX: 1000, translateX: 1000, duration: 4, repeatDelay: 2, className: "h-12", },
+    { initialX: 1200, translateX: 1200, duration: 6, repeatDelay: 4, delay: 2, className: "h-6", },
+];
 
 export const BeamsWithCollision = ({ className, }: { className?: string; }) => {
     const containerRef = useRef<HTMLDivElement>(null!);
     const parentRef = useRef<HTMLDivElement>(null!);
-
-    const beams = [
-        {
-            initialX: 10,
-            translateX: 10,
-            duration: 7,
-            repeatDelay: 3,
-            delay: 2,
-        },
-        {
-            initialX: 600,
-            translateX: 600,
-            duration: 3,
-            repeatDelay: 3,
-            delay: 4,
-        },
-        {
-            initialX: 100,
-            translateX: 100,
-            duration: 7,
-            repeatDelay: 7,
-            className: "h-6",
-        },
-        {
-            initialX: 400,
-            translateX: 400,
-            duration: 5,
-            repeatDelay: 14,
-            delay: 4,
-        },
-        {
-            initialX: 800,
-            translateX: 800,
-            duration: 11,
-            repeatDelay: 2,
-            className: "h-20",
-        },
-        {
-            initialX: 1000,
-            translateX: 1000,
-            duration: 4,
-            repeatDelay: 2,
-            className: "h-12",
-        },
-        {
-            initialX: 1200,
-            translateX: 1200,
-            duration: 6,
-            repeatDelay: 4,
-            delay: 2,
-            className: "h-6",
-        },
-    ];
 
     return (
         <div
