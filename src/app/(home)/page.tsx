53d86736ace98aafacc39ebab4b8be0b71c7c910
pphatdev <pphat.kits@gmail.com>
2026-05-13 17:49:52 +0700

    feat: implement home page sections with custom UI components, skill visualization, and site-wide layout elements

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,9 @@ import Footer from "@/shared/components/layouts/footer";
 import HeroSection from "./hero";
 import { SectionSkills } from "./skills";
 import { GotoTop } from "@/shared/components/layouts/goto-top";
+import { SectionAboutMe } from "./about";
+import { SectionProjects } from "@/app/(home)/projects";
+import { SectionExperience } from "./experience";
 
 export default function Home() {
     return (
@@ -13,8 +16,17 @@ export default function Home() {
             {/* Hero Section */}
             <HeroSection />
 
+            {/* About Me Section */}
+            <SectionAboutMe />
+
             {/* Skill Section */}
             <SectionSkills />
+
+            {/* Projects Section */}
+            <SectionProjects />
+
+            {/* Experience Section */}
+            <SectionExperience />
 
             {/* Goto Top Section */}
             <GotoTop />
