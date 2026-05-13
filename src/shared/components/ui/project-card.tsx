3e8dbe10b3a95bcfa0a68e957ec23b3684027a23
pphatdev <pphat.kits@gmail.com>
2026-05-13 17:50:25 +0700

    feat: implement shared project cards, resizable navbar, titles, and supporting utility components and hooks

---
+++ src/shared/components/ui/project-card.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Badge } from "@/shared/components/ui/badge";
+import Link from "next/link";
+import { ExternalLinkIcon, GlobeIcon } from "lucide-react";
+import AvatarCircles from "@/shared/components/ui/avatar-circles";
+import Image from "next/image";
+import React from "react";
+import { cn } from "@/shared/libs/utils";
+import { Contributor, Project } from "@/shared/interfaces/projects";
+
+
+const ProjectCard: React.FC<{ project: Project, className?: string }> = ({ project, className }) => {
+    const avatars = project?.contributors?.map((contributor: Contributor) => ({
+        imageUrl: contributor.avatarUrl,
+        profileUrl: contributor.url,
+        title: contributor.name,
+    }));
+
+    return (
+        <div
+            className={cn("col-span-1 top-5 sticky duration-300 drop-shadow-xl drop-shadow-black/2 overflow-hidden max-sm:p-1 p-0.5 max-sm:rounded-none bg-background group font-sans rounded-4xl mb-4 ring-1 ring-foreground/5 hover:ring-primary/50 hover:ring-2 transition-all ease-in-out flex flex-col h-full", className)}
+            role="article"
+            tabIndex={-1}>
+
+            <Image src={project.thumbnail} width={512} height={512} alt={project.title || ""} className="object-cover w-full aspect-video duration-300 transition-all ease-in-out max-sm:rounded-none rounded-4xl" />
+
+            <div className='bg-background/30 ring-1 w-fit absolute top-4 right-4 ml-auto ring-foreground/10 justify-end flex rounded-full max-sm:rounded-none p-1'>
+                {/* {project.tags?.map((tag, index) => (
+                    <Link
+                        key={index}
+                        target="_blank"
+                        href={tag.tag}
+                        aria-label={tag.tag}
+                        title={tag.tag}
+                        className="flex max-sm:rounded-none rounded-full p-2 hover:ring ring-foreground/20 hover:bg-background/40 text-foreground/90 hover:text-foreground transition-all items-center justify-center">
+                        <ExternalLinkIcon className="size-4" />
+                    </Link>
+                ))} */}
+
+                <Link
+                    target="_blank"
+                    href={''}
+                    className="flex max-sm:rounded-none rounded-full p-2 hover:ring ring-foreground/20 hover:bg-background/40 text-foreground/90 hover:text-foreground transition-all items-center justify-center">
+                    <ExternalLinkIcon className="size-4" />
+                </Link>
+            </div>
+
+            <h2 className="text-lg px-4 mt-5 z-10 font-semibold font-sans leading-snug tracking-tight line-clamp-2 pb-1">{project.title}</h2>
+            <header className='mb-2 px-4 relative flex mt-2 justify-between items-center'>
+                <div className="flex gap-2 items-center">
+                    {project.tags?.slice(0, 3).map((tag, index) => (
+                        <Badge key={index} variant="outline" className="text-xs border border-primary/50">{tag.tag}</Badge>
+                    ))}
+                </div>
+            </header>
+
+            <p className='font-normal px-4 z-10 line-clamp-2 text-foreground/80'>
+                {project.description}
+            </p>
+
+            <footer className="mt-auto px-4 pb-4 flex justify-between pt-2 z-10">
+                <div className='bg-foreground/5 ring-1 w-fit ring-foreground/10 justify-end flex  max-sm:rounded-none rounded-full p-1'>
+                    <AvatarCircles numPeople={avatars.length - 4} avatarUrls={avatars} />
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default ProjectCard;
